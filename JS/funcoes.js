28d5c47250d42a8082bcbedf3d5e604e70954d1c
rafaelmronqui <rafaelmronqui@gmail.com>
2022-07-03 15:41:52 -0300

    Teste

---
+++ JS/funcoes.js
@@ -0,0 +1,13 @@
+function mostrarCincoAlertsComFor()
+{
+	recebeNumero = parseInt(prompt("Digite um valor"));
+	
+	for(numero=1; numero <= recebeNumero; numero++)
+	{
+		alert (numero);
+	}
+}
+
+
+
+
